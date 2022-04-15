b463836a815e461a4e7e2f51913199fd04b66896
kperryman <101473071+kperryman@users.noreply.github.com>
2022-04-15 14:57:30 -0700

    Added #12

---
+++ 12.py
@@ -0,0 +1,7 @@
+A=input("What is your age?")
+A=int(A)
+if A>=16: print("You can drive")
+if A>=18: print("You can vote")
+if A>=21: print ("You can enjoy a beer")
+if A>=55: print ("You get the senior discount")
+else: print("You are so young")
